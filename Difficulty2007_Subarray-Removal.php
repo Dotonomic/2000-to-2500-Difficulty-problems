2070c110f2f71a5b3af3ac14a7e5b9eddebdc66a
Dotonomic <github@dotonomic.com>
2024-03-02 19:52:59 +0000

    Comments added

--- Difficulty2007_Subarray-Removal.php
+++ Difficulty2007_Subarray-Removal.php
@@ -9,13 +9,15 @@ for ($case = 0; $case < NumCases; $case++) {
     $n = readline();
     
     $permutation = explode(' ', readline());
-    
+
+    // Call the recursive scoring function with the position of the value 1 in the permutation as the position to cut.
     echo score($n, $permutation, array_search(1, $permutation))."\n";
 }
 
 function score($n, $array, $cut) {
     $nPos = array_search($n, $array);
-    
+
+    // Slice/cut the current array and replace it with the resulting subarray that contains N.
     if ($nPos > $cut)
         $array = array_slice($array, $cut + 1);
     else
@@ -28,7 +30,8 @@ function score($n, $array, $cut) {
     $min = $n - $length + 1;
     
     $dist = $middle + 1;
-    
+
+    // Set the next cut to the position closest to the middle of the current array for which the value is too low, if there is one.
     foreach ($array as $key => $value)
         if ($value < $min)
             if (abs($middle - $key) < $dist) {
@@ -38,10 +41,12 @@ function score($n, $array, $cut) {
             }
             else
                 break;
-    
+
+    // If a next cut is set, call the scoring function with arguments N, Current array, Next cut;
     if (isset($nextCut))
         return score($n, $array, $nextCut);
-            
+    
+    // otherwise, no value in the current array is too low, so it is the solution. Return its length as the score.
     return $length;
 }
 
